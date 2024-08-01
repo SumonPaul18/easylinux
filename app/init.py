3ea735b056467dbbba37311a99d25eb7d9864b66
sumonpaul18 <sumonpaul461@gmail.com>
2024-08-01 22:55:03 +0600

    delete all

--- app/init.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-import os
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')  # Replace with a strong secret key
-
-from app import routes
