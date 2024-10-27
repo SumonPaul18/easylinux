2eb5ca425056196fc9a090fb4051b78f3c612d9d
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-10-27 15:28:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ def index():
         
         file_path = os.path.join(folder_name, 'data.txt')
         with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(f'NAME= "{input1}"\n')
-            file.write(f'EMAIL= "{input2}"\n')
-            file.write(f'DEPT= "{input3}"\n')
+            file.write(f'NAME="{input1}"\n')
+            file.write(f'EMAIL="{input2}"\n')
+            file.write(f'DEPT="{input3}"\n')
         # run shell script
         script_path = './shell.sh'
         result = subprocess.run(['bash', script_path, input1, input2, input3], capture_output=True, text=True)
@@ -32,4 +32,4 @@ def index():
     return render_template('index.html', output=output)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
