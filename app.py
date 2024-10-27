f17ee978a4dd00155258e3af924b773b03a194af
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-10-27 11:39:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
-from flask import Flask, render_template, request
-import paramiko
+from flask import Flask, render_template, request, flash
+import os
+import subprocess
 
 app = Flask(__name__)
-
-def ssh_command(ip_address, username, password, command):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip_address, username=username, password=password)
-
-    stdin, stdout, stderr = ssh.exec_command(command)
-    output = stdout.read().decode('utf-8')
-    error = stderr.read().decode('utf-8')
-    ssh.close()
-
-    return output, error
+app.secret_key = 'your_secret_key'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    output = None
     if request.method == 'POST':
-        ip_address = request.form['ip_address']
-        username = request.form['username']
-        password = request.form['password']  # Securely handle passwords
-        command = request.form['command']
-
-        output, error = ssh_command(ip_address, username, password, command)
-        # Now return the index template with output and error variables
-        return render_template('index.html', output=output, error=error)
-
-    return render_template('index.html')
+        input1 = request.form['input1']
+        input2 = request.form['input2']
+        input3 = request.form['input3']
+        
+        folder_name = 'data_folder'
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        
+        file_path = os.path.join(folder_name, 'data.txt')
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(f'NAME= "{input1}"\n')
+            file.write(f'EMAIL= "{input2}"\n')
+            file.write(f'DEPT= "{input3}"\n')
+        # run shell script
+        script_path = './shell.sh'
+        result = subprocess.run(['bash', script_path, input1, input2, input3], capture_output=True, text=True)
+        output = result.stdout
+        
+        flash('Data saved successfully!')
+    
+    return render_template('index.html', output=output)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
